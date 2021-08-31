c25398fcec6bed5b489b6d6400b24201b32d7185
syphix99 <arthur99.adriaens@gmail.com>
2021-09-01 01:17:42 +0200

    pandas notities toegevoegd

--- google_crash_course.py
+++ google_crash_course.py
@@ -1,26 +1,16 @@
 import numpy as np
 import pandas as pd
 
-# Create and populate a 5x2 NumPy array.
-my_data = np.array([[0, 3], [10, 7], [20, 9], [30, 14], [40, 15]])
+#genereer een 3x4 pandas DataFrame met kolommen Eleanor, Chidi, Tahani en Jason
+data = np.random.randint(101, size=(3,4))
+KolomNamen = ['Eleanor','Chidi','Tahani','Jason']
+DataFrame = pd.DataFrame(data=data,columns=KolomNamen)
 
-# Create a Python list that holds the names of the two columns.
-my_column_names = ['temperature', 'activity']
+#print voll dataframe:
+print(DataFrame)
 
-# Create a DataFrame.
-my_dataframe = pd.DataFrame(data=my_data, columns=my_column_names)
+#print waarde van 1ste rij van de Eleanor kolom:
+print(DataFrame['Eleanor'].iloc[[1]])
 
-# Create a new column named adjusted.
-my_dataframe["adjusted"] = my_dataframe["activity"] + 2
-
-print("Rows #0, #1, and #2:")
-print(my_dataframe.head(3), '\n')
-
-print("Row #2:")
-print(my_dataframe.iloc[[2]], '\n')
-
-print("Rows #1, #2, and #3:")
-print(my_dataframe[1:4], '\n')
-
-print("Column 'temperature':")
-print(my_dataframe['temperature'])
+#maak een 5de kolom Janet die de waarde heeft van Tahani+Jason:
+DataFrame['Janet'] = DataFrame['Tahani'] + DataFrame['Jason']
